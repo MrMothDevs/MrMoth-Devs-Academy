7868e01f8f8115626074ccdf3504fc4e8e3947ad
MrCoku <65773022+ElProfesor-dev@users.noreply.github.com>
2021-06-05 00:34:12 +0200

    navbar

---
+++ views/js/scroll.js
@@ -0,0 +1,16 @@
+var scrollBtn = document.getElementById("scrollBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
